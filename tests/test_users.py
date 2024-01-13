14a1328376c915ae92a410970f50fdbb345a6717
oyollobrian <oyollobrian@gmail.com>
2024-01-13 07:13:43 +0300

    test unique username creation

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,5 @@
 import pytest
-from app import schemas, models
+from app import schemas, models, utils
 
 
 def test_get_all_users(client):
@@ -53,3 +53,26 @@ def test_nonexistent_user(client):
     response = client.get("/users/132322442")
     assert response.status_code == 404
 
+
+def test_create_users_with_similar_display_names(client):
+    # usernames should be unique
+    dummy_user1 = {"email":'testuser@gmail.com', "display_name":'TestUser', "password":"Password123"}
+    dummy_user2 = {"email":'testuser1@gmail.com', "display_name":'TestUser', "password":"Password123"}
+
+    response1 = client.post("/users/new",json=dummy_user1)
+    created_user = response1.json()
+    first_username = created_user['username']
+
+    response2 = client.post("/users/new",json=dummy_user2)
+    created_user2 = response2.json()
+    second_username = created_user2['username']
+ 
+    assert response1.status_code == 201
+    assert response2.status_code == 201
+    assert created_user['email'] == dummy_user1['email']
+    assert created_user['display_name'] == dummy_user1['display_name']
+    assert created_user['password'] == dummy_user1['password']
+    assert created_user2['email'] == dummy_user2['email']
+    assert created_user2['display_name'] == dummy_user2['display_name']
+    assert created_user2['password'] == dummy_user2['password']
+    assert second_username != first_username
